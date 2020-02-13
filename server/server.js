91f48362b7e3fa89c793cb2442c973b2ab198d5e
christophernixon <chrisnixon31@gmail.com>
2020-02-13 22:25:47 +0000

    Removed the usage of the express backend until the issue with deploying to heroku is resolved.

--- server/server.js
+++ server/server.js
@@ -1,19 +1,14 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8080;
-const bodyParser = require('body-parser')
-app.use(express.static(path.join(__dirname, 'build')));
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
 
-app.get('/ping', function (req, res) {
-    return res.send('pong');
-});
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 app.listen(port, () => {
-   console.log("Server is up on port %s.",port);
+   console.log('Server is up on port %s!',port);
 });
-// app.listen(process.env.PORT || 8080);
