2c95f9053acaa89790f01eb92fda9017e7e53b85
christophernixon <chrisnixon31@gmail.com>
2020-02-14 12:10:40 +0000

    demo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edited App.js! (save to reload).
+          Demo edited.
         </p>
         <a
           className="App-link"
