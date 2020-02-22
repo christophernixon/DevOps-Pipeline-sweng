2169807a2897b7671d4cbae7cfdaf6e6a4835047
Davy Nolan <dnolan5@tcd.ie>
2020-02-22 17:38:28 +0000

    Added "start game" button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import React from "react";
+import "./App.css";
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Demo edited.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button className="Button" onClick={startGame}>
+          Start Game!
+        </button>
+      </header>{" "}
     </div>
   );
+}
+
+function startGame(){
+  console.log("Start game button pressed!")
 }
 
 export default App;
