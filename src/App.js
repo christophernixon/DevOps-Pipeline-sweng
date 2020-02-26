e50d4ed977fb6794f28b80e4b709b4981249fa39
Hannah Mahon <mahonha@tcd.ie>
2020-02-26 11:00:34 +0000

    map feature added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
 import logo from './logo.svg';
+import React, { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
 import './App.css';
+import './map.jsx';
 
 function App() {
+  const [viewport, setViewport] = useState({
+    width: "100vw",
+    height: "100vh",
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 10
+  });
+
+  console.log(process.env.REACT_APP_MAPBOX_TOKEN)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactMapGL
+      {...viewport}
+      mapboxApiAccessToken = {process.env.REACT_APP_MAPBOX_TOKEN}
+      onViewportChange={setViewport}
+    />
   );
 }
 
