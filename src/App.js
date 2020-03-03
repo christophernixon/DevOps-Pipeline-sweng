17035309e643419b133530a0e1db43595fdeb23f
christophernixon <chrisnixon31@gmail.com>
2020-03-03 21:42:36 +0000

    Refactored App function into class, added dragable custom marker.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,63 @@
-// eslint-disable-next-line
-import logo from './logo.svg';
-import React, { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import React, { Component } from 'react';
+import ReactMapGL, { Marker } from 'react-map-gl';
+import green_marker from './images/green_marker.png';
 import './App.css';
 import './map.jsx';
 
-function App() {
-  const [viewport, setViewport] = useState({
-    width: "100vw",
-    height: "100vh",
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 10
-  });
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        latitude: 37.8,
+        longitude: -122.4,
+        zoom: 10,
+        width: "100vw",
+        height: "100vh",
+        bearing: 0,
+        pitch: 0
+      },
+      marker: {
+        latitude: 37.8,
+        longitude: -122.4,
+      }
+    };
+  }
 
-  console.log(process.env.REACT_APP_MAPBOX_TOKEN)
+  _onMarkerDragEnd = event => {
+    this.setState({
+      marker: {
+        latitude: event.lngLat[1],
+        longitude: event.lngLat[0],
+      }
+    });
+  };
 
-
-
-  return (
-    <ReactMapGL
-      {...viewport}
-      mapboxApiAccessToken = {process.env.REACT_APP_MAPBOX_TOKEN}
-      onViewportChange={setViewport}
-    />
-
-  );
+  render() {
+    const marker_style = {
+      backgroundRepeat: "no-repeat",
+      width: 25,
+      height: 35,
+    };
+    const {viewport, marker} = this.state;
+    return (
+      <ReactMapGL
+        {...viewport}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        onViewportChange={viewport => this.setState({ viewport })}>
+        <Marker 
+        latitude={marker.latitude} 
+        longitude={marker.longitude} 
+        draggable={true}
+        onDragEnd={this._onMarkerDragEnd} >
+          <img src={green_marker} style={marker_style} alt="green_marker"></img>
+        </Marker>
+      </ReactMapGL>);
+  }
 }
 // eslint-disable-next-line
-function startGame(){
+function startGame() {
   console.log("Start game button pressed!")
 }
 
