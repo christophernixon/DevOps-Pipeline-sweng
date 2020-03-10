a492f00e39b254eb3b49b68babe81888360d4657
Davy Nolan <45881685+davynolan1@users.noreply.github.com>
2020-03-10 17:38:30 +0000

    Added docker files for creating a docker image (#34)
    ## Pipeline
    * Containerized application with Docker.
    * Added first smoke test
    * Added files for eslint and added scripts for linting
    * Added Travis CI badge to show status of develop branch
    
    ## Frontend
    * Refactored App function into class, added dragable custom marker.
    * Added text popup
    * Changed zoom level
    
    ## Documentation
    * Added Design Specification, Architectural_Decision and Project Plan documents.
    
    Co-authored-by: christophernixon <chrisnixon31@gmail.com>
    Co-authored-by: John O'Doherty <odoherj1@tcd.ie>
    Co-authored-by: christophernixon <32682045+christophernixon@users.noreply.github.com>
    Co-authored-by: Chris Staunton <stauntch@tcd.ie>
    Co-authored-by: hannahmahon1 <47337923+hannahmahon1@users.noreply.github.com>
    Co-authored-by: paul ocallaghan <ocallap1@tcd.ie>

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DevOps-Pipeline-sweng
+![](https://travis-ci.com/christophernixon/DevOps-Pipeline-sweng.svg?branch=develop)
+
 A project aimed at implementing a CI/CD pipeline for a simple reactJS application. 
---
+++ src/components/CustomPopup.js
@@ -0,0 +1,25 @@
+  import React from 'react';  
+  import './style.css';  
+
+  class CustomPopup extends React.Component {  
+    render() {  
+  return (  
+  <div className='popup'>
+
+
+    
+
+  </div>
+
+  
+
+
+
+
+  
+  );  
+  }  
+  }  
+  
+  export default CustomPopup;
+
