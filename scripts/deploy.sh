b941a8689f05228d806ca0f1bd749f6656c8bb23
christophernixon <chrisnixon31@gmail.com>
2020-03-15 11:36:49 +0000

    Changed namespace and repository name so they use '-' instead of '_' to prevent errors from ccreating a Deployment using kubectl. Added coloring to information logs for readability.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,34 +1,38 @@
 #!/bin/bash
 # Usage : chmod +x ./scripts/* && ./scripts/deploy.sh <Container Registry Namespace>
 
+# Setting variables for output coloring
+mag=$'\e[1;35m'
+end=$'\e[0m'
+
 cr_namespace=$1
 run_locally=$2
 cr_endpoint="uk.icr.io"
 cluster_name="develop_cluster"
 deployment_name="sweng-devops-develop-deployment"
-printf "**********************Variables**********************"
-printf "Container Registry Namespace: $cr_namespace\n"
-printf "Container Registry API endpoint: $cr_endpoint\n"
-printf "Cluster name: $cluster_name\n"
-printf "Deployment name: $deployment_name\n"
+printf "${mag}**********************Variables**********************\n${end}"
+printf "${mag}Container Registry Namespace: $cr_namespace\n${end}"
+printf "${mag}Container Registry API endpoint: $cr_endpoint\n${end}"
+printf "${mag}Cluster name: $cluster_name\n${end}"
+printf "${mag}Deployment name: $deployment_name\n${end}"
 
 # Download and install the IBM cloud CLI tools.
-printf "*****\nInstalling IBM Cloud CLI\n*****\n"
+printf "${mag}*****\nInstalling IBM Cloud CLI\n*****\n${end}"
 curl -sL https://ibm.biz/idt-installer | bash
 
 # Logging into the IBM Cloud environment using env variable of API key
-printf "*****\nLogging into IBM Cloud using apikey\n*****\n"
+printf "${mag}*****\nLogging into IBM Cloud using apikey\n*****\n${end}"
 ibmcloud login -a https://api.eu-gb.bluemix.net --apikey $DEVOPS_IBM_KEY
 if [ $? -ne 0 ]; then
-  printf "*****\nFailed to authenticate to IBM Cloud\n*****\n"
+  printf "${mag}*****\nFailed to authenticate to IBM Cloud\n*****\n${end}"
   exit 1
 fi
 
 # Logging into the IBM Cloud container registry
-printf "*****\nLogging into IBM Cloud container registry\n*****\n"
+printf "${mag}*****\nLogging into IBM Cloud container registry\n*****\n${end}"
 ibmcloud cr login
 if [ $? -ne 0 ]; then
-  printf "*****\nFailed to authenticate to IBM Cloud container registry\n*****\n"
+  printf "${mag}*****\nFailed to authenticate to IBM Cloud container registry\n*****\n${end}"
   exit 1
 fi
 
@@ -36,15 +40,15 @@ fi
 DEPLOY_TIMESTAMP=`date +'%Y%m%d-%H%M%S'`
 
 # Build the docker image, tag it with a custom tag and push it to the given CR namespace
-printf "*****\nBuilding image, tagging as $DEPLOY_TIMESTAMP-$TRAVIS_BUILD_NUMBER-$TRAVIS_BRANCH and latest\n*****\n"
+printf "${mag}*****\nBuilding image, tagging as $DEPLOY_TIMESTAMP-$TRAVIS_BUILD_NUMBER-$TRAVIS_BRANCH and latest\n*****\n${end}"
 docker build --tag $cr_endpoint/$cr_namespace/$cr_namespace:$DEPLOY_TIMESTAMP-$TRAVIS_BUILD_NUMBER-$TRAVIS_BRANCH .
-docker tag $cr_endpoint/$cr_namespace/$cr_namespace:$DEPLOY_TIMESTAMP-$TRAVIS_BUILD_NUMBER-$TRAVIS_BRANCH $cr_endpoint/$cr_namespace/develop:latest
+docker tag $cr_endpoint/$cr_namespace/$cr_namespace:$DEPLOY_TIMESTAMP-$TRAVIS_BUILD_NUMBER-$TRAVIS_BRANCH $cr_endpoint/$cr_namespace/$cr_namespace:latest
 
 # Push built image to Container Registry
-printf "*****\nPushing image to container registry\n*****\n"
+printf "${mag}*****\nPushing image to container registry\n*****\n${end}"
 docker push $cr_endpoint/$cr_namespace/$cr_namespace:$DEPLOY_TIMESTAMP-$TRAVIS_BUILD_NUMBER-$TRAVIS_BRANCH
 if [ $? -ne 0 ]; then
-  printf "*****\nFailed to push image to IBM Cloud container registry, quota may be exceeded.\n*****\n"
+  printf "${mag}*****\nFailed to push image to IBM Cloud container registry, quota may be exceeded.\n*****\n${end}"
   ibmcloud cr quota
   exit 1
 else
@@ -56,11 +60,11 @@ fi
 #############################################
 
 if [ $run_locally == 'run-locally' ]; then
-    printf "*****\nInstalling kubectl using Homebrew.\n*****\n"
+    printf "${mag}*****\nInstalling kubectl using Homebrew.\n*****\n${end}"
     brew install kubectl
     brew upgrade kubernetes-cli
 else
-    printf "*****\nInstalling kubectl\n*****\n"
+    printf "${mag}*****\nInstalling kubectl\n*****\n${end}"
     curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
     chmod +x ./kubectl
     sudo mv ./kubectl /usr/local/bin/kubectl
@@ -71,25 +75,24 @@ ibmcloud plugin update kubernetes-service
 export IKS_BETA_VERSION=1
 
 # Configure our pre-existing cluster.
-printf "*****\nConfiguring cluster\n*****\n"
+printf "${mag}*****\nConfiguring cluster\n*****\n${end}"
 ibmcloud ks cluster config --cluster $cluster_name
 
 # Remove previous deployment and create new deployment
-printf "*****\nCreating deployment on cluster pointing to image $cr_endpoint/$cr_namespace/$cr_namespace:latest\n*****\n"
+printf "${mag}*****\nCreating deployment on cluster pointing to image $cr_endpoint/$cr_namespace/$cr_namespace:latest\n*****\n${end}"
 kubectl delete deployment $deployment_name
 kubectl create deployment $deployment_name --image=$cr_endpoint/$cr_namespace/$cr_namespace:latest
 
 # Expose the deployment on port 3000
-printf "*****\nExposing the deployment on port 3000\n*****\n"
+printf "${mag}*****\nExposing the deployment on port 3000\n*****\n${end}"
 kubectl expose deployment/$deployment_name --type="NodePort" --port 3000
 
 # Install jq for processing json from ibmcloud cli
-printf "*****\nInstalling jq\n*****\n"
-if [ $run_locally == 'run-locally' ]; then
+printf "${mag}*****\nInstalling jq\n*****\n${end}"
+if [ "$run_locally" == "run-locally" ]; then
     brew install jq
 else
     sudo apt-get install jq
-    chmod +x jq
 fi
 
 # Extract the public IP for the cluster
@@ -97,4 +100,4 @@ public_ip=$(ibmcloud ks workers $cluster_name -json | jq -r '.[0].publicIP')
 # Extract the NodePort of the kubernetes service
 nodeport=$(kubectl get service $deployment_name -o json | jq -r '.spec.ports[0].nodePort')
 
-printf "*****\nThe application can now be viewed at http://$public_ip:$nodeport/\n*****\n"
+printf "${mag}*****\nThe application can now be viewed at http://$public_ip:$nodeport/\n*****\n${end}"
