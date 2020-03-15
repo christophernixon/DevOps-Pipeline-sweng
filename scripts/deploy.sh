11aecacc4e89d2dad359eb11e1b37f9131529324
christophernixon <chrisnixon31@gmail.com>
2020-03-15 13:10:25 +0000

    Improved logging with a new logging function

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # Usage : chmod +x ./scripts/* && ./scripts/deploy.sh <Container Registry Namespace>
 
-# Setting variables for output coloring
+# Setting up colored outputs
 mag=$'\e[1;35m'
 end=$'\e[0m'
+log_info () {
+  printf "${mag}*****\n${end}"
+  printf "${mag}$1${end}"
+  printf "${mag}*****\n${end}"
+}
 
 cr_namespace=$1
 run_locally=$2
@@ -14,25 +19,25 @@ printf "${mag}**********************Variables**********************\n${end}"
 printf "${mag}Container Registry Namespace: $cr_namespace\n${end}"
 printf "${mag}Container Registry API endpoint: $cr_endpoint\n${end}"
 printf "${mag}Cluster name: $cluster_name\n${end}"
-printf "${mag}Deployment name: $deployment_name\n${end}"
+printf "${mag}Deployment name: $deployment_name\n\n${end}"
 
 # Download and install the IBM cloud CLI tools.
-printf "${mag}*****\nInstalling IBM Cloud CLI\n*****\n${end}"
+log_info "Installing IBM Cloud CLI\n"
 curl -sL https://ibm.biz/idt-installer | bash
 
 # Logging into the IBM Cloud environment using env variable of API key
-printf "${mag}*****\nLogging into IBM Cloud using apikey\n*****\n${end}"
+log_info "Logging into IBM Cloud using apikey\n"
 ibmcloud login -a https://api.eu-gb.bluemix.net --apikey $DEVOPS_IBM_KEY
 if [ $? -ne 0 ]; then
-  printf "${mag}*****\nFailed to authenticate to IBM Cloud\n*****\n${end}"
+  log_info "Failed to authenticate to IBM Cloud\n"
   exit 1
 fi
 
 # Logging into the IBM Cloud container registry
-printf "${mag}*****\nLogging into IBM Cloud container registry\n*****\n${end}"
+log_info "Logging into IBM Cloud container registry\n"
 ibmcloud cr login
 if [ $? -ne 0 ]; then
-  printf "${mag}*****\nFailed to authenticate to IBM Cloud container registry\n*****\n${end}"
+  log_info "Failed to authenticate to IBM Cloud container registry\n"
   exit 1
 fi
 
@@ -40,15 +45,15 @@ fi
 DEPLOY_TIMESTAMP=`date +'%Y%m%d-%H%M%S'`
 
 # Build the docker image, tag it with a custom tag and push it to the given CR namespace
-printf "${mag}*****\nBuilding image, tagging as $DEPLOY_TIMESTAMP-$TRAVIS_BUILD_NUMBER-$TRAVIS_BRANCH and latest\n*****\n${end}"
+log_info "Building image, tagging as $DEPLOY_TIMESTAMP-$TRAVIS_BUILD_NUMBER-$TRAVIS_BRANCH and latest\n"
 docker build --tag $cr_endpoint/$cr_namespace/$cr_namespace:$DEPLOY_TIMESTAMP-$TRAVIS_BUILD_NUMBER-$TRAVIS_BRANCH .
 docker tag $cr_endpoint/$cr_namespace/$cr_namespace:$DEPLOY_TIMESTAMP-$TRAVIS_BUILD_NUMBER-$TRAVIS_BRANCH $cr_endpoint/$cr_namespace/$cr_namespace:latest
 
 # Push built image to Container Registry
-printf "${mag}*****\nPushing image to container registry\n*****\n${end}"
+log_info "Pushing image to container registry\n"
 docker push $cr_endpoint/$cr_namespace/$cr_namespace:$DEPLOY_TIMESTAMP-$TRAVIS_BUILD_NUMBER-$TRAVIS_BRANCH
 if [ $? -ne 0 ]; then
-  printf "${mag}*****\nFailed to push image to IBM Cloud container registry, quota may be exceeded.\n*****\n${end}"
+  log_info "Failed to push image to IBM Cloud container registry, quota may be exceeded.\n"
   ibmcloud cr quota
   exit 1
 else
@@ -60,14 +65,14 @@ fi
 #############################################
 
 if [ $run_locally == 'run-locally' ]; then
-    printf "${mag}*****\nInstalling kubectl using Homebrew.\n*****\n${end}"
-    brew install kubectl
-    brew upgrade kubernetes-cli
+ log_info "Installing kubectl using Homebrew.\n"
+  brew install kubectl
+  brew upgrade kubernetes-cli
 else
-    printf "${mag}*****\nInstalling kubectl\n*****\n${end}"
-    curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-    chmod +x ./kubectl
-    sudo mv ./kubectl /usr/local/bin/kubectl
+  log_info "Installing kubectl\n"
+  curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+  chmod +x ./kubectl
+  sudo mv ./kubectl /usr/local/bin/kubectl
 fi
 
 # Make sure that ibmcloud kubernetes-service is up to date
@@ -75,20 +80,20 @@ ibmcloud plugin update kubernetes-service
 export IKS_BETA_VERSION=1
 
 # Configure our pre-existing cluster.
-printf "${mag}*****\nConfiguring cluster\n*****\n${end}"
+log_info "Configuring cluster\n"
 ibmcloud ks cluster config --cluster $cluster_name
 
 # Remove previous deployment and create new deployment
-printf "${mag}*****\nCreating deployment on cluster pointing to image $cr_endpoint/$cr_namespace/$cr_namespace:latest\n*****\n${end}"
+log_info "Creating deployment on cluster pointing to image $cr_endpoint/$cr_namespace/$cr_namespace:latest\n"
 kubectl delete deployment $deployment_name
 kubectl create deployment $deployment_name --image=$cr_endpoint/$cr_namespace/$cr_namespace:latest
 
 # Expose the deployment on port 3000
-printf "${mag}*****\nExposing the deployment on port 3000\n*****\n${end}"
+log_info "Exposing the deployment on port 3000\n"
 kubectl expose deployment/$deployment_name --type="NodePort" --port 3000
 
 # Install jq for processing json from ibmcloud cli
-printf "${mag}*****\nInstalling jq\n*****\n${end}"
+log_info "Installing jq\n"
 if [ "$run_locally" == "run-locally" ]; then
     brew install jq
 else
@@ -100,4 +105,4 @@ public_ip=$(ibmcloud ks workers $cluster_name -json | jq -r '.[0].publicIP')
 # Extract the NodePort of the kubernetes service
 nodeport=$(kubectl get service $deployment_name -o json | jq -r '.spec.ports[0].nodePort')
 
-printf "${mag}*****\nThe application can now be viewed at http://$public_ip:$nodeport/\n*****\n${end}"
+log_info "The application can now be viewed at http://$public_ip:$nodeport/\n"
