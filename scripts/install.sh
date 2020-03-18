6c9627dd57a21c4172ef5173f5e697d10067aaba
christophernixon <32682045+christophernixon@users.noreply.github.com>
2020-03-18 21:55:27 +0000

    Closes #53, Adding production deployment environment to pipeline. (#56)
    * Added ability to log into either develop or production IBM accounts based on command line arguments and Travis CI env variables., changed deploy script so that it will work for both develop and production environments, again based on command-line arguments.

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,16 +9,18 @@ log_info () {
   printf "${mag}*****\n${end}"
 }
 
-run_locally=$1
+run_locally="false"
+environment=$1
 printf "${mag}**********************Variables**********************\n${end}"
 printf "${mag}Running script locally: $run_locally\n${end}"
+printf "${mag}Logging into environment: $environment\n${end}"
 
 # Download and install the IBM cloud CLI tools.
 log_info "Installing IBM Cloud CLI\n"
 curl -sL https://ibm.biz/idt-installer | bash
 
 # Download and install kubectl
-if [ "$run_locally" == "run-locally" ]; then
+if [ "$run_locally" == "true" ]; then
  log_info "Installing kubectl using Homebrew.\n"
   brew install kubectl
   brew upgrade kubernetes-cli
@@ -31,15 +33,25 @@ fi
 
 # Install jq for processing json from ibmcloud cli
 log_info "Installing jq\n"
-if [ "$run_locally" == "run-locally" ]; then
-    brew install jq
+if [ "$run_locally" == "true" ]; then
+  brew install jq
 else
-    sudo apt-get install jq
+  sudo apt-get install jq
+fi
+
+# Choosing which api key to use depending on whether develop or prod environment is being used.
+if [ "$environment" == "develop" ]; then
+  API_KEY=$DEVOPS_IBM_KEY
+elif [ "$environment" == "production" ]; then
+  API_KEY=$DEVOPS_IBM_PROD_KEY
+else
+  log_info "Unable to identify targeted environment. Given environment: $environment\n"
+  exit 1
 fi
 
 # Logging into the IBM Cloud environment using env variable of API key
 log_info "Logging into IBM Cloud using apikey\n"
-ibmcloud login -a https://api.eu-gb.bluemix.net --apikey $DEVOPS_IBM_KEY
+ibmcloud login -a https://api.eu-gb.bluemix.net --apikey $API_KEY
 if [ $? -ne 0 ]; then
   log_info "Failed to authenticate to IBM Cloud\n"
   exit 1
