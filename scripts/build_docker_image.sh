6025559eb2ac97234fd37dbcba2f4c60d0842076
christophernixon <chrisnixon31@gmail.com>
2020-03-19 00:39:26 +0000

    Due to docker images not being cached between jobs, the image will need to be build, tagged and pushed within one job

--- scripts/build_docker_image.sh
+++ scripts/build_docker_image.sh
@@ -11,14 +11,16 @@ log_info () {
 
 cr_namespace=$1
 cr_endpoint="uk.icr.io"
+cr_repository="sweng-devops"
 printf "${mag}**********************Variables**********************\n${end}"
 printf "${mag}Container Registry Namespace: $cr_namespace\n${end}"
+printf "${mag}Container Registry Repository: $cr_repository\n${end}"
 printf "${mag}Container Registry API endpoint: $cr_endpoint\n${end}"
 
 # Build the docker image, tag it with a custom tag and push it to the given CR namespace
 log_info "Building image, tagging as $TRAVIS_BUILD_NUMBER-$TRAVIS_BRANCH and latest\n"
-docker build --tag $cr_endpoint/$cr_namespace/$cr_namespace:$TRAVIS_BUILD_NUMBER-$TRAVIS_BRANCH .
-docker tag $cr_endpoint/$cr_namespace/$cr_namespace:$TRAVIS_BUILD_NUMBER-$TRAVIS_BRANCH $cr_endpoint/$cr_namespace/$cr_namespace:latest
+docker build --tag $cr_endpoint/$cr_namespace/$cr_repository:$TRAVIS_BUILD_NUMBER-$TRAVIS_BRANCH .
+docker tag $cr_endpoint/$cr_namespace/$cr_repository:$TRAVIS_BUILD_NUMBER-$TRAVIS_BRANCH $cr_endpoint/$cr_namespace/$cr_repository:latest
 log_info "Displaying docker images\n"
 docker images
 
@@ -30,7 +32,6 @@ log_info "Pushing image to container registry\n"
 docker push $cr_endpoint/$cr_namespace/$cr_repository:$TRAVIS_BUILD_NUMBER-$TRAVIS_BRANCH
 if [ $? -ne 0 ]; then
   log_info "Failed to push image to IBM Cloud container registry, quota may be exceeded.\n"
-  ibmcloud cr quota
   exit 1
 else
   docker push $cr_endpoint/$cr_namespace/$cr_repository:latest
