5b11945cb5b97738b5a318fc2b88a24c1eb11863
Chris Staunton <stauntch@tcd.ie>
2020-03-21 17:53:48 +0000

    reworked to allow multiple popups

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -7,11 +7,9 @@ class Popup extends React.Component {
       <div className='popup'>
         <div className='popup\_inner'>
           <div>{this.props.text}</div>
-          <button className='button' onClick={() => {
-            this.props.nextQ()
-            this.props.submitA()
-          }}>Submit Answer</button>
-          <button className='button' onClick={this.props.closePopup}>Close</button>
+          {this.props.submitA}
+          {this.props.closePopup}
+          {this.props.endGame}
         </div>
       </div>
     );
