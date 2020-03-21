f573c2cadff03e5570e624296c4f03a3b534aff2
christophernixon <32682045+christophernixon@users.noreply.github.com>
2020-03-21 23:21:50 +0000

    Revert "Version1.1 (#69)"
    This reverts commit 32544ec93951672abb8a428a493e258600563120.

--- cypress/integration/popup.spec.js
+++
@@ -1,7 +0,0 @@
-describe('Popup', function() {
-    it('closes on clicking Close button', function() {
-        cy.visit('/') 
-        cy.contains('Start game').click()
-        cy.get('.popup').contains('Close').click()
-    })
-  })
