140593f9ff71055526d2338760f410091ba3f6da
christophernixon <chrisnixon31@gmail.com>
2020-03-21 17:40:22 +0000

    Readded necessary import for testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import App from './App';
 import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import distanceCalculator from './distance';
 
 test('renders without crashing', () => {
