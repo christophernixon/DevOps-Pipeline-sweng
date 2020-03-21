580a73eabc84fe94e8c40d51af419cd229d65169
Chris Staunton <stauntch@tcd.ie>
2020-03-21 14:23:15 +0000

    added distanceCalculator test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 import ReactDOM from 'react-dom'
+import distanceCalculator from './distance'
+import Popup from './components/Popup';
 
 test('renders without crashing', () => {
   const div = document.createElement('div');
@@ -20,3 +22,11 @@ it('renders button correctly', () => {
   const {getByTestId} = render(<button data-testid= 'button'>Next Question</button>)
   expect(getByTestId('button').textContent).toBe("Next Question")
 })
+
+it('distance funtion returns correct value', () => {
+  
+  const distanceAnswer = 1433
+  const dist = distanceCalculator(41.890220642089844, 12.492316246032715, 51.50807571411133, -0.09714961051940918)
+  expect(Math.floor(dist)).toBe(distanceAnswer)
+})
+
